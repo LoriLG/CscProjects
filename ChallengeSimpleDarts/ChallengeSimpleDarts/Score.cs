94ffb0e94515070fb259862a5d49c972386ef0f8
LLG52931@gmail.com <LLG52931@gmail.com>
2017-08-31 11:07:11 -0700

    Added Darts Challenge

---
+++ ChallengeSimpleDarts/ChallengeSimpleDarts/Score.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Darts;
+
+namespace ChallengeSimpleDarts
+{
+    public class Score
+    {
+        public static void ScoreDart(Player player, Dart dart)
+        {
+            int score = 0;
+
+            if (dart.IsTripleBand)
+                score = dart.Score * 3;
+            else if (dart.IsDoubleBand)
+                score = dart.Score * 2;
+            else if (dart.IsOuterBullsEye)
+                score = 25;
+            else score = dart.Score;
+
+
+            if (dart.IsTripleBand && dart.Score == 0)
+                score = 50;
+            //else if (dart.Score == 0)
+              // score = 25;
+
+            
+            player.Score += score;
+
+        }
+    }
+}
---
+++ ChallengeSimpleDarts/Darts/Dart.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darts
+{
+    public class Dart
+    {
+      
+
+        public int Score { get; set; }
+        public bool IsDoubleBand { get; set; }
+        public bool IsTripleBand { get; set; }
+        public bool IsOuterBullsEye {get;set;}
+
+        private Random _random;
+
+        public Dart(Random random)
+        {
+            _random = random;
+        }
+
+        public void Throw()
+        {
+           Score = _random.Next(0, 21);
+           if (Score == 0)
+                IsOuterBullsEye = true;
+
+           int multiplier = _random.Next(1, 101);
+           if (multiplier > 95)
+                 IsTripleBand = true;
+           else if (multiplier > 90)
+                 IsDoubleBand = true; 
+
+        }
+            
+    }
+}
+
