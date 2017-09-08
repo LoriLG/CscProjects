2a068e3a00dc9ca8152c3f460dc2d646f5424d74
LLG52931@gmail.com <LLG52931@gmail.com>
2017-09-07 19:55:17 -0700

    Added MegaChallenge War

---
+++ MegaChallengeWar/MegaChallengeWar/Deck.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+
+namespace MegaChallengeWar
+{
+    public class Deck
+    {
+        private List<Card> _deck = new List<Card>();
+        private Random _random = new Random();
+        StringBuilder _sb = new StringBuilder();
+    
+        public Deck()
+        {
+         
+            string[] suits = new string[]{ "Diamonds", "Hearts", "Spades", "Clubs" };
+            string[] kinds = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+            foreach (var suit in suits)
+            {
+                foreach(var kind in kinds)
+                { 
+                
+                    _deck.Add(new Card() { Suit = suit, CardName = kind });
+                }
+            }
+
+        }
+
+        public string Deal(Player player1, Player player2)
+        {
+            while(_deck.Count > 0)
+            {
+                dealCard(player1);
+                dealCard(player2);
+            }
+            return _sb.ToString();
+         }
+
+        private void dealCard(Player player)
+        {
+            Card card = _deck.ElementAt(_random.Next(_deck.Count));
+            player.Cards.Add(card);
+            _deck.Remove(card);
+
+            _sb.Append("<br/>");
+            _sb.Append(player.PlayerName);
+            _sb.Append(" is dealt the ");
+            _sb.Append(card.CardName);
+            _sb.Append(" of ");
+            _sb.Append(card.Suit);
+        }
+    }
+}
---
+++ MegaChallengeWar/MegaChallengeWar/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MegaChallengeWar.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    Play War !<form id="form1" runat="server">
+        <p>
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Play" />
+        </p>
+        <p>
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
